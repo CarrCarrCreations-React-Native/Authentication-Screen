fae75ad54b920054aa0668738e350aa50844c660
Liam <liam@liamcarr.com>
2018-09-03 00:21:50 -0400

    Create LoginForm Framework
    Created a LoginForm component to hold a Card with 3 CardSections.
    
    important reusable components
    
    set up the framework for firebase auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import firebase from 'firebase';
+import {Header} from './components/common';
+import LoginForm from "./components/LoginForm";
+
 
 export default class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAJkrr4mq4FTIjwCMmgc5gK-zhohOUhekc",
+            authDomain: "aythentication-80ac0.firebaseapp.com",
+            databaseURL: "https://aythentication-80ac0.firebaseio.com",
+            projectId: "aythentication-80ac0",
+            storageBucket: "aythentication-80ac0.appspot.com",
+            messagingSenderId: "712755268277"
+        });
+    }
+
     render() {
         return (
             <View>
-                <Text>An app</Text>
+                <Header headerText={"Authentication"}/>
+                <LoginForm/>
             </View>
         );
     }
